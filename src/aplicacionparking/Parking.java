66b77cdecc6d0323402502fd6de0f0143563c659
christianbegines <christianbeginescarrasco@gmail.com>
2016-01-25 09:23:16 +0100

    subida 2

--- src/aplicacionparking/Parking.java
+++ src/aplicacionparking/Parking.java
@@ -20,26 +20,37 @@ public class Parking {
         Plaza p1= new Plaza(1,1);
         Plaza p2= new Plaza(2,1);
         Plaza p3= new Plaza(3,1);
+        
         Plaza p4= new Plaza(4,1);
+        
         Plaza p5= new Plaza(1,2);
         Plaza p6= new Plaza(2,2);
-        Plaza p7= new Plaza(3,3);
-        Plaza p8= new Plaza(4,3);
-       
-        
-        
+ 
+        Plaza p7= new Plaza(3,2);
+        Plaza p8= new Plaza(4,2);
+        listaPlazas.put(p1.SNN(),p1);
+        listaPlazas.put(p2.SNN(),p2);
+        listaPlazas.put(p3.SNN(),p3);
+        listaPlazas.put(p4.SNN(),p4);
+        listaPlazas.put(p5.SNN(),p5);
+        listaPlazas.put(p6.SNN(),p6);
+        listaPlazas.put(p7.SNN(),p7);
+        listaPlazas.put(p8.SNN(),p8);
     }
+    
     public String alquilar(Vehiculo v){
         Iterator<String> it =listaPlazas.keySet().iterator();
         boolean comprobar=false;
-        String clave,resp="";
-        Plaza p;
-        while( it.hasNext()) {
-            clave = it.next();
-             p = listaPlazas.get(clave);
-            if(p.getVehiculo()==null){
-                resp=p.SNN();
-            }else resp=null;
+        String numPlaza,resp=null;
+        Plaza plaza;
+        while( it.hasNext() && comprobar==false) {
+            numPlaza = it.next();
+             plaza = listaPlazas.get(numPlaza);
+            if(plaza.getVehiculo()==null){
+                comprobar=true;
+                resp=plaza.SNN();
+                plaza.setVehiculo(v);
+            }
         }return resp;
     }
     
